9c4bfbbb7b0fcbb2985043cbe7605804cb7dac51
Ana Benites <anitabenites2014@gmail.com>
2018-07-05 15:24:02 +0200

    first version

--- server.js
+++ server.js
@@ -13,20 +13,20 @@ console.log(shortid.generate());
 
 
 let event1 = {
-  _id: shortid.generate(),
+  "id": shortid.generate(),
   "title": "Volleyball",
   "description": "Outdoor sport",
   "date": "29-06-2018"
 }
 let event2 = {
-  _id: shortid.generate(),
+  "id": shortid.generate(),
   "title": "Swimming",
   "description": "Outdoor sport",
   "date": "29-06-2018"
 }
 
 let event3 = {
-  _id: shortid.generate(),
+  "id": shortid.generate(),
   "title":"Football",
   "description": "Outdoor sport",
   "date":"29-06-2018"
@@ -51,22 +51,50 @@ app.get('/events/:id', (req,res) => {
 });
 
 app.post('/events', function(req,res) {
-  var id = shortid.generate();
-  var title = req.body.title;
-  var description = req.body.description;
-  var date = req.body.date;
-  res.send({id,title, description,date});
+  var newEvent = {
+    "id": shortid.generate(),
+    "title": req.body.title,
+    "description": req.body.description,
+    "date": req.body.date
+  }
+  var num = Object.keys(events).length + 1;
+  events[num] = newEvent;
+  res.send(events[num]);
 });
 
-app.patch('/events/:id/', function(req,res) { // updating information in the url
-if(req.params.id in events) {
-  var title = req.body.title;
-  var description = req.body.description;
-  var date = req.body.date;
-  res.send(events[req.params.id], title, description, date);
-}
+// there should be an endpoint that allows to update an existing event(PATCH/events/{})
+app.patch('/events/:id', function(req, res) {
+  if(req.params.id in events) {
+    if(req.body.title) {
+      // console.log("Footbal", req.body.title);
+      // console.log(req.body.title);
+      // console.log(events[req.params.id]['title']);
+      // console.log("Previous title", events[req.params.id]['title']);
+      events[req.params.id]['title'] = req.body.title;
+      // console.log("Title changed", events[req.params.id]['title']);
+    } else if(req.body.description) {
+      // console.log("New", req.body.description);
+      // console.log("Old", req.params.id.description);
+      events[req.params.id]['description'] = req.body.description;
+    } else if(req.body.date) {
+        events[req.params.id]['date'] = req.body.date;
+    }
+    res.send(events[req.params.id]); // why it does not work with a ".notation"?
+  } else {
+    res.send(404);
+  }
 });
 
+app.delete('/events/:id', function(req,res) {
+  if(req.params.id in events) {
+    console.log("this is:", req.params.id)
+    delete events[req.params.id]
+    res.send(events);
+  } else {
+    res.send(404);
+  }
+
+});
 
 app.post('/', (req,res) => res.send('Got a POST request'));
 
