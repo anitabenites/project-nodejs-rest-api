3af7a588d389b72a2aa643e803d547cc87cffd93
Ana Benites <anitabenites2014@gmail.com>
2018-07-17 15:55:14 +0200

    correcting a testing unit

--- test/test.js
+++ test/test.js
@@ -1,16 +1,19 @@
 
 var chai = require('chai');
-var expect = chai.expect;
+var expect = chai.expect; // i can access to this submodul!
 var chaiHttp = require('chai-http');
 // var request = chai.request;
 
 chai.use(chaiHttp);
 
+
 describe('Endpoint', function() {
+  //You should implement a test for the endpoint that returns all the events
     it('should return the same events', function() {
       expect('./events').to.equal('./events');
     });
 
+    //Use chai-http to start and make requests to your server
     it('Use chai-http to start and make requests to your server', function(done){
       chai.request('http://localhost:3000') // it works when the server is running otherwise no!
         .get('/events')
@@ -21,7 +24,8 @@ describe('Endpoint', function() {
         });
       });
 
-      it('testing the put events http request created with mocha', function() {
+      //You should implement a test for the endpoint that creates an event
+      it('testing the put events http request created with mocha', function(done) {
         chai.request('http://localhost:3000')
         .post('/events') // endpoint!
         .send({
@@ -31,7 +35,52 @@ describe('Endpoint', function() {
           'description': 'req.body.description',
           'date': 'req.body.date'
         })
+        .end(function(err,res){
+          expect(res).to.have.status(200);
+          expect(err).to.be.null
+          done();
+        })
       });
-});
 
-//You should implement a test for the endpoint that creates an event
+      //You should implement a test for the endpoint that returns one event by its id
+      it('testing the endpoint that returns one event by its id', function(done) {
+        chai.request('http://localhost:3000')
+        .get('/events/2') // endpoint!
+        .end(function(err,res){
+          expect(res).to.have.status(200);
+          expect(err).to.be.null
+          done();
+        })
+      });
+      //You should implement a test for the endpoint that updates an existing event
+      it('updating an existing event', function(done){
+        chai.request('http://localhost:3000')
+        .patch('/events/2')
+        .send({
+          '_method': 'put',
+          'id': '2134',
+          'title': 'soccer',
+          'description': 'with friends',
+          'date': '22-09-2018'
+        })
+        .end(function(err,res){
+          expect(res).to.have.status(200);
+          expect(err).to.be.null
+          done();
+        })
+      })
+
+      //You should implement a test for the endpoint that deletes an existing event
+      it('delete an existing event', function(done){
+        chai.request('http://localhost:3000')
+        .delete('/events/2')
+        .send('the event has been delete')
+        .end(function(err,res){ // this is a callback!
+          expect(res).to.have.status(200);
+          expect(err).to.be.null;
+          done();
+        })
+      })
+
+
+});
